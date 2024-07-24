bb3fb7e10c5d5af1be078ed2a7b9a361d0ed9679
Victor <ban.victor728@gmail.com>
2024-07-24 09:00:11 -0400

    react router dom tab config done. adding react router dom to main, importing routes & route, then adding it to main section & writing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
-function App() {
+import { Routes,Route } from 'react-router-dom'
+
+const App = () => {
   
 
   return (
     <>
-      
+      <div id="container">
+
+        <div id="navbar">{/* navigation here */}</div>
+
+        <div id="main-section">
+           <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+          </div>
+      </div>
     </>
   )
 }
